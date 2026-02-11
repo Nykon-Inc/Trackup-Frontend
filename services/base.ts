dadf92f9a33d4850d8257e52d5c0a9a2dafb742b
Ahman Emmanuel <ahmanemmanuel@gmail.com>
2026-02-11 13:10:47 +0100

    log base url to be sure

--- services/base.ts
+++ services/base.ts
@@ -32,7 +32,7 @@ class HttpFacade {
     }
 
     private async setupRequirements() {
-
+        console.log(this.baseUrl)
         let isRefreshing = false;
         let refreshPromise: Promise<string> | null = null;
         let failedQueue: {
