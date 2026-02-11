3da1034316938dbbae2e0d1553c0d728e3b2818d
Ahman Emmanuel <ahmanemmanuel@gmail.com>
2026-02-11 13:16:31 +0100

    redeploy

--- services/base.ts
+++ services/base.ts
@@ -32,7 +32,6 @@ class HttpFacade {
     }
 
     private async setupRequirements() {
-        console.log("BASE URL", this.baseUrl)
         let isRefreshing = false;
         let refreshPromise: Promise<string> | null = null;
         let failedQueue: {
